f0633a436af8d1d84ff322f4a0b774f40ef2a07c
Marcelo <marcelovvodrigues@gmail.com>
2021-04-18 19:55:39 -0300

    Add Bin2Dec_Convert

---
+++ Bin2Dec_Convert/Conversor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bin2Dec_Convert
+{
+    public class Conversor
+    {
+        public float binInDec(string numBin)
+            {
+                int expo = 0;
+                int numero;
+                float soma = 0;
+                string numeroInvertido = StrReverse(numBin);
+
+                for (int i = 0; i < numeroInvertido.Length; i++)
+                {
+                    numero = Convert.ToInt32(numeroInvertido.Substring(i, 1));
+                    soma += numero * (int)Math.Pow(2, expo);
+                    expo++;
+                }
+                return soma;
+            }
+        //function revert to string
+        public static string StrReverse(string str)
+            {
+                return new string(str.Reverse().ToArray());
+            }
+    }
+}
---
+++ Bin2Dec_Convert/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Bin2Dec_Convert
+{
+    static class Program
+    {
+        static void Main()
+        {
+            int calculo = 0;
+            Conversor convert = new Conversor();
+            string valor = null;
+
+            Console.WriteLine("Iniciando conversor binario para decimal! (para finalizar digite x)");
+
+            //Condição Loop
+            while (valor != "x")
+            {
+                Console.WriteLine("Informe o numero binário:");
+                valor = Console.ReadLine();
+            
+                //Validação
+                if(valor == "x")
+                {
+                    Console.WriteLine($"Finalizando... Calculos:{calculo}");
+                }
+                else
+                {
+                    //Calculo
+                    float result = convert.binInDec(valor);
+                    Console.WriteLine($"Numero Binario: {valor}. Resultado Decimal: {result}");
+                    calculo++;
+                }
+            }
+        }
+    }
+}
