d1b0542cf244d6e75065c09b300368ac11ec9352
Marcelo <marcelovvrodrigues@gmail.com>
2021-05-23 14:20:53 -0300

    Add AluraTunes - Linq

---
+++ Alura/AluraTunes/AluraTunes/LinqToObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraTunes
+{
+    class LinqToObject
+    {
+        public void teste()
+        {
+            var generos = new List<Genero>
+            {
+                new Genero {Id=1,Nome="Rock"},
+                new Genero {Id=2,Nome="Reggae"},
+                new Genero {Id=3,Nome="Rock Progressivo"},
+                new Genero {Id=4,Nome="Punk Rock"},
+                new Genero {Id=5,Nome="Clássica"}
+            };
+
+            foreach (var genero in generos)
+            {
+                Console.WriteLine($"{genero.Id}\t,{genero.Nome}");
+            }
+
+            //select * fromgeneros
+
+            var query = from g in generos where g.Nome.Contains("Rock") select g;
+            foreach (var genero in query)
+            {
+                Console.WriteLine($"{genero.Id}\t,{genero.Nome}");
+            }
+
+
+            //listar musicas
+            var musicas = new List<Musica>
+            {
+                new Musica { Id = 1, Nome = "Sweet Child O'Mine", GeneroId = 1 },
+                new Musica { Id = 2, Nome = "I Shoot The Sheriff", GeneroId = 2},
+                new Musica { Id = 3, Nome = "Danúbio Azul", GeneroId = 5}
+            };
+
+            var musicaQuery = from m in musicas join g in generos on m.GeneroId equals g.Id select new { m, g };
+
+            foreach (var musica in musicaQuery)
+            {
+                Console.WriteLine($"{musica.m.Id}\t{musica.m.Nome}\t{musica.g.Nome}");
+            }
+
+            Console.ReadKey();
+        }
+
+        class Genero
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+        }
+
+        class Musica
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public int GeneroId { get; set; }
+        }
+    }
+}
+
---
+++ Alura/AluraTunes/AluraTunes/LinqToXml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AluraTunes
+{
+    class LinqToXml
+    {
+        public void Teste()
+        {
+            XElement root = XElement.Load("Automoveis.xml");
+
+            var queryXML = from g in root.Element("Generos").Elements("Genero") select g;
+
+            foreach (var genero in queryXML)
+            {
+                Console.WriteLine("{0}\t{1}", genero.Element("GeneroId").Value, genero.Element("Nome").Value);
+            }
+
+            var query = from g in root.Element("Generos").Elements("Genero")
+                        join m in root.Element("Musicas").Elements("Musica")
+                            on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
+                        select new
+                        {
+                            Musica = m.Element("Nome").Value,
+                            Genero = g.Element("Nome").Value
+                        };
+            Console.WriteLine();
+            foreach (var musicaGenero in query)
+            {
+                Console.WriteLine("{0}\t{1}", musicaGenero.Musica, musicaGenero.Genero);
+            }
+        }
+    }
+}
