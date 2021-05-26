27198dc513fdd56fb1f21c1558df9fbcbbd7e168
Marcelo <marcelovvrodrigues@gmail.com>
2021-05-26 19:55:44 -0300

    Add c# certif. pt1

---
+++ certificacao-csharp-pt1/certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/2 - Métodos Substituídos/2 - Métodos Substituídos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class MetodosSubstituidos : IAulaItem
+    {
+        public void Executar()
+        {
+            Animal gato = new Gato() { Nome = "Bichano" };
+            gato.Beber();
+            gato.Comer();
+            gato.Andar();
+
+            var gato2 = new Gato() { Nome = "Bichano2" };
+            gato2.Beber();
+            gato2.Comer();
+            gato2.Andar();
+        }
+    }
+
+    class Animal
+    {
+        public String Nome { get; set; }
+
+        public void Beber()
+        {
+            Console.WriteLine("Animal.Beber");
+        }
+
+        public virtual void Comer()
+        {
+            Console.WriteLine("Animal.Comer");
+        }
+
+        public void Andar()
+        {
+            Console.WriteLine("Animal.Andar");
+        }
+    }
+
+    class Gato : Animal
+    {
+        public new void Beber()
+        {
+            Console.WriteLine("Gato.Beber");
+        }
+
+        public override void Comer()
+        {
+            Console.WriteLine("Gato.Comer");
+        }
+
+        public new void Andar()
+        {
+            Console.WriteLine("Gato.Andar");
+        }
+    }
+}
