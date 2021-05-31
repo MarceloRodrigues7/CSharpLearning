b631e5040895e34bf937e89ea5c44465fa130328
Marcelo <marcelovvrodrigues@gmail.com>
2021-05-30 22:01:08 -0300

    Add c# certif. pt1
    continuando

---
+++ Alura/certificacao-csharp/certificacao-csharp-pt1/Aula5 - box unbox para converter/2 - Unboxing/Unboxing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.Aula5___box_unbox_para_converter._2___Unboxing
+{
+    class Unboxing
+    {
+        public void teste()
+        {
+            int numero = 57;
+            object caixa = numero;
+            try
+            {
+                int unboxed = (int)caixa;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Alura/certificacao-csharp/certificacao-csharp-pt1/Aula6 - Cast de Tipos/ConversoesImplicitas/ConversoesImplicitas.cs
@@ -0,0 +1,28 @@
+﻿using certificacao_csharp_roteiro.antes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.Aula6___Cast_de_Tipos.ConversoesImplicitas
+{
+    class ConversoesImplicitas
+    {
+        public void teste()
+        {
+            int numero = 2_123_456_789;
+            long inteiroLongo = numero;
+            Console.WriteLine(inteiroLongo);
+
+            //numero = inteiroLongo;
+
+            Gato gato = new Gato();
+            Animal animal = gato;
+            Console.WriteLine(animal.GetType());
+            IAnimal ianimal = gato;
+            Console.WriteLine(ianimal.GetType());
+            //gato = animal;
+        }
+    }
+}
