09d553ebb735648d8b29940a910d9097f9ca27e1
Marcelo Rodrigues <57776491+MarceloRodrigues7@users.noreply.github.com>
2022-02-03 15:33:25 -0300

    Exemplos Actions & ActionChaining

---
+++ ActionEChaining/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExemplosActions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = methodOne;
+            action += methodTwo;
+            action += () => methodThree("teste parametro");
+            action += () => methodFour();
+            action += () => methodFour("teste default");
+            action();
+            Console.ReadKey();
+        }
+
+        static void methodOne()
+        {
+            Console.WriteLine("10-10-1000");
+        }
+        static void methodTwo()
+        {
+            Console.WriteLine("20-02-2000");
+        }
+        static void methodThree(string value)
+        {
+            Console.WriteLine(value);
+        }
+        static void methodFour(string value = "defaut")
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
