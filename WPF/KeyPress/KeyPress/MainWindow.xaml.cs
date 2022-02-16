96dcfee493a8f0cf41f713d91685da40e9470e45
Marcelo Rodrigues <marcelovvrodrigues@gmail.com>
2022-02-16 08:15:36 -0300

    WPF - Estudos tecla pressionada

---
+++ WPF/KeyPress/KeyPress/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyPress
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Key _keyPress;
+        private string Texto = "Tecla Pressionada: ";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            _keyPress = e.Key;
+        }
+
+        private void btnValida_Click(object sender, RoutedEventArgs e)
+        {
+            txtResp.Text = Texto + _keyPress.ToString();
+        }
+    }
+}
