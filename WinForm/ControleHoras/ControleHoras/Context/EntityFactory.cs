5fe4061f40c069888f9e65b672d801dfc0c4baf0
Marcelo Rodrigues <marcelovvrodrigues@gmail.com>
2022-02-22 19:52:02 -0300

    .

--- WinForm/ControleHoras/ControleHoras/Context/EntityFactory.cs
+++ WinForm/ControleHoras/ControleHoras/Context/EntityFactory.cs
@@ -10,7 +10,7 @@ namespace ControleHoras.Context
 {
     public class EntityFactory : DbContext
     {
-        
+        private readonly string connectionsString = @"Data Source=143.244.190.244;Initial Catalog=ControleHoras;User ID='sa';Password='P@ssword';Connect Timeout=999899999";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(connectionsString);
 
         public DbSet<Dia> Dias { get; set; }
