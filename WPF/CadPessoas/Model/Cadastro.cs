202048ccb9cb26fbba67708a1dc0afb0ac2729cd
Marcelo Rodrigues <marcelovvrodrigues@gmail.com>
2022-02-24 17:00:09 -0300

    Learn MVVM

---
+++ WPF/CadPessoas/Model/Cadastro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadPessoas.Model
+{
+    public class Cadastro : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _nome;
+        private string _sobrenome;
+        private string _cidade;
+
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+                OnPropertyChanged("Nome");
+            }
+        }
+
+        public string Sobrenome
+        {
+            get { return _sobrenome; }
+            set
+            {
+                _sobrenome = value;
+                OnPropertyChanged("Sobrenome");
+            }
+        }
+
+        public string Cidade
+        {
+            get { return _cidade; }
+            set
+            {
+                _cidade = value;
+                OnPropertyChanged("Cidade");
+            }
+        }
+
+    }
+}
---
+++ WPF/CadPessoas/ViewModel/CadastroViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CadPessoas.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadPessoas.ViewModel
+{
+    public class CadastroViewModel : ObservableCollection<Cadastro>
+    {
+        public CadastroViewModel()
+        {
+            CadastrosCollection();
+        }
+
+        private void CadastrosCollection()
+        {
+            var cadastroUm = new Cadastro
+            {
+                Cidade = "São Paulo",
+                Nome = "José",
+                Sobrenome = "Silva"
+            };
+            var cadastroDois = new Cadastro
+            {
+                Cidade = "Rio de Janeiro",
+                Nome = "Ana",
+                Sobrenome = "Maria"
+            };
+            var cadastroTres = new Cadastro
+            {
+                Cidade = "São Paulo",
+                Nome = "João",
+                Sobrenome = "Costa"
+            };
+            Add(cadastroUm);
+            Add(cadastroDois);
+            Add(cadastroTres);
+        }
+    }
+}
