3f57d3cf502e8391ffde4c0d57dfe5d2192540a4
Marcelo Rodrigues <marcelovvrodrigues@gmail.com>
2022-03-06 18:48:40 -0300

    OO

---
+++ SchoolOfNet/POO/Conta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Conta
+    {
+        private int _agencia;
+        private int _numeroConta;
+        private decimal _saldo;
+
+        public int Agencia
+        {
+            get { return _agencia; }
+            private set
+            {
+                if (value > 0)
+                    _agencia = value;
+            }
+        }
+        public int NumeroConta { get => _numeroConta; set => _numeroConta = value; }
+        public decimal Saldo { get => _saldo; set => _saldo = value; }
+
+        public void SetAgencia(int agencia) => _agencia = agencia;
+        public void Sacar(decimal valor) => _saldo -= valor;
+        public void Depositar(decimal valor) => _saldo += valor;
+    }
+}
---
+++ SchoolOfNet/POO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente cc = new();
+            cc.SetAgencia(1029);
+            Console.WriteLine(cc.Agencia);
+            
+            ContaPoupanca cp = new();
+
+            Console.ReadKey();
+        }
+    }
+}
