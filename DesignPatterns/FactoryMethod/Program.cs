78ab8741565bc31bf15f5675e2a6ef2ea2533249
marcelo <marcelovvrodrigues@gmail.com>
2023-05-15 23:06:54 -0300

    Init DesignPatterns pluralsight

---
+++ DesignPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using static FactoryMethod.Implementation;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factories = new List<DiscountFactory> {
+            new CodeDiscountFactory(Guid.NewGuid()),
+            new CountryDiscountFactory("BR") };
+
+            foreach (var factory in factories)
+            {
+                var discountService = factory.CreateDiscountService();
+                Console.WriteLine($"Percentage {discountService.DiscountPercentage} coming from {discountService}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance1 = Logger.Instance;
+            var instance2 = Logger.Instance;
+
+            if (instance1 == instance2 && instance2 == Logger.Instance)
+            {
+                Console.WriteLine("Instances are the same.");
+            }
+
+            instance1.Log($"Message from {nameof(instance1)}");
+            // or
+            instance1.Log($"Message from {nameof(instance2)}");
+            // or
+            Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+        }
+    }
+}
