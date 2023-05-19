ce78d0afff71ae75f3dc254f4130f11f6cdafed1
marcelo <marcelovvrodrigues@gmail.com>
2023-05-19 07:53:14 -0300

    Continuação abstractFactory e Builder

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using static AbstractFactory.Implementation;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+            var shoppingCartForBelgium = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+            shoppingCartForBelgium.CalculateCosts();
+
+            var franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+            var shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+            shoppingCartForFrance.CalculateCosts();
+
+            Console.ReadKey();
+        }
+    }
+}
